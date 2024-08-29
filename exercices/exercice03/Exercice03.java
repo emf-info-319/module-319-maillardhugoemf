190d7d0d965928c9e2145acbc3976ae2da014031
maillardhugoemf <hugo.maillard@studentfr.ch>
2024-08-29 11:20:51 +0200

    exercice03

---
+++ exercices/exercice03/Exercice03.java
@@ -0,0 +1,9 @@
+package exercices.exercice03;
+
+public class Exercice03 {
+public static void main(String[] args) {
+    System.out.println("je m'apelle hugo et j'ai 15 ans");
+    System.out.println("dans 10 ans j'aurai 25 ans");
+    }
+}    
+
