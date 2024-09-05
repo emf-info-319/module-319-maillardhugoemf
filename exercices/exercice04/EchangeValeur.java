041a9345d7cf87d264f2e964a67db7ef0889b40f
maillardhugoemf <hugo.maillard@studentfr.ch>
2024-09-05 09:15:50 +0200

    exercice4

--- exercices/exercice04/EchangeValeur.java
+++ exercices/exercice04/EchangeValeur.java
@@ -1,8 +1,24 @@
 package exercices.exercice04;
 
 public class EchangeValeur {
-public static void main(String[] args) {
-  
-}
+    public static void main(String[] args) {
+        int variable1 = 1;
+        int variable2 = 2;
+
+
+        System.out.println("le contenu de la variable1 est "+variable1+" le contenu de la variable2 est"+ variable2 );
+
+        int variable3 = variable1;
+        variable1=variable2; //valeur1 = 2
+        variable2=variable3; //valeur2 = 2
+ 
+    
+    System.out.println("le contenu de la variable1 est"+ variable1);
+   System.out.println("le contenu de la variable2 est"+ variable2);
+    System.out.println(".... traitement.... ");
+    System.out.println("le contenus de la variable1 est"+ variable1);
+    System.out.println("le contenus de la variable2 est"+ variable3);
+
 }
 
+}
---
+++ teste.java
@@ -0,0 +1,8 @@
+public class teste {
+public static void main(String[] args) {
+    int age = 18
+    System.out.println("j'ai "+ age + "ans);
+    sys
+    }
+
+}
