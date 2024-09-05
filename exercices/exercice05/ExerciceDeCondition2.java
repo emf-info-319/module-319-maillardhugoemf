357794fd6ec5aa4b60e0686d2217edf1f9cdb17c
maillardhugoemf <hugo.maillard@studentfr.ch>
2024-09-05 09:41:53 +0200

    exercice conditin  2

--- exercices/exercice05/ExerciceDeCondition2.java
+++ exercices/exercice05/ExerciceDeCondition2.java
@@ -1,5 +1,12 @@
 package exercices.exercice05;
 
 public class ExerciceDeCondition2 {
-    
+    public static void main(String[] args) {
+     int valeurATester = 7;
+     if (valeurATester %2 == 0 ) {System.out.println("le nombre est paire");
+        
+     } else {System.out.println("le nombre est impaire");
+        
+     }    
+    }
 }
