90378a03b638eb5a726f1e02b61d557ae1a35a06
maillardhugoemf <hugo.maillard@studentfr.ch>
2024-09-05 09:48:14 +0200

    exercice condition 3

---
+++ exercices/exercice05/exerciceDeCondition3.java
@@ -0,0 +1,15 @@
+package exercices.exercice05;
+
+public class exerciceDeCondition3 {
+    public static void main(String[] args) {
+        
+        int valeur1 = 5;
+        int valeur2 = -9; 
+    if (valeur1 + valeur2 >0 ) {System.out.println("le nombre est positif");
+        
+    } else {System.out.println("le nombre est negatif");
+        
+    }
+    
+    }
+}
