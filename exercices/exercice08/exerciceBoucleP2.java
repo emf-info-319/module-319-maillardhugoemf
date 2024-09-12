b53442aeb84a76e770e3742f2a6b901ad9f77d2d
maillardhugoemf <hugo.maillard@studentfr.ch>
2024-09-12 10:52:28 +0200

    exercice boucle

--- exercices/exercice08/exerciceBoucleP2.java
+++ exercices/exercice08/exerciceBoucleP2.java
@@ -3,20 +3,11 @@ package exercices.exercice08;
 public class exerciceBoucleP2 {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
-            System.out.println("Boucle for, itération" + i);
+            if (i == 3) {
+            } 
+            else {
+                System.out.println("Boucle for itération" + i);
+            }
         }
-        int nombre = 1;
-        while (nombre <= 5) {
-            System.out.println("Boucle while itération" + nombre);
-            nombre++;
-        }
-            int nombre2 = 0;  
-            do 
-                nombre2++;
-                System.out.println("Boucle do-while, itération" + nombre2);
-            // le nombre2++ = a nombre + 1 jusqu'a nombre2= 5
-            while (nombre2 < 5);
-
-        
     }
 }
---
+++ exercices/exercice08/exerciceboucle1p3.java
@@ -0,0 +1,14 @@
+package exercices.exercice08;
+
+public class exerciceboucle1p3 {
+    public static void main(String[] args) {
+        int nombre = 0;
+        while (nombre <= 5) {
+            nombre++;
+            if (nombre >= 4) {
+            } else {
+                System.out.println("Boucle while itération" + nombre);
+            }
+        }
+    }
+}
