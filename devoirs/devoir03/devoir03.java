1c547708ed236bac91d9b0b7a4fe02f653eea5ca
hugo <hugo.maillard@studentfr.ch>
2024-09-24 21:37:48 +0200

    devoir03

---
+++ devoirs/devoir03/devoir03.java
@@ -0,0 +1,10 @@
+package devoirs.devoir03;
+
+public class devoir03 {
+    public static void main(String[] args) {
+      int jour = 24;
+      int mois = 9; 
+      int annee = 2024;
+      System.out.println("on est le " + jour + "/" + mois + "/" + annee);  
+    }// je n'est pas trouver le bon code 
+}
