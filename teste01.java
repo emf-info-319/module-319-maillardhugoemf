a930548793a24ba36b8e57465e95e9d04c1eaed6
maillardhugoemf <hugo.maillard@studentfr.ch>
2024-09-26 13:49:52 +0200

    testeo1

--- teste01.java
+++ teste01.java
@@ -3,15 +3,36 @@ public class teste01 {
         int age = 2;
         if (age >= 18 && age < 100) {
             System.out.println("je suis majeur");
-        } 
+        }
         if (age >= 100) {
             System.out.println("je suis un dinosaure");
         }
-        if (age >= 200 ) { 
+        if (age >= 200) {
             System.out.println("meme un super dinosaure");
-        }
-        else {
+        } else {
             System.out.println("je suis mineur");
+        }            
+        int equipe1 = (int) (Math.random() * (4 - 1)) + 0;
+        int equipe2 = (int) (Math.random() * (4 - 1)) + 0;
+        int equipe3 = (int) (Math.random() * (4 - 1)) + 0;
+        int equipe4 = (int) (Math.random() * (4 - 1)) + 0;
+        if (equipe1 < equipe2) {
+            System.out.println(" equipe 1 est éliminer " + equipe1 + "  " + equipe2);
         }
+        if (equipe2 < equipe1) {
+            System.out.println("equipe2 est eliminer " + equipe1 + " " + equipe2);
+        }
+        else if (equipe2 == equipe1 ) {
+        equipe1 = (int) (Math.random() * (5 - 1)) + 0;
+        equipe2 = (int) (Math.random() * (5 - 1)) + 0;
+        if (equipe1 < equipe2) {
+            System.out.println(" equipe 1 est éliminer " + equipe1 + "  " + equipe2);
+        }
+        if (equipe1 < equipe2) {
+            System.out.println(" equipe 1 est éliminer " + equipe1 + "  " + equipe2);
+        }
+        else
+        {System.out.println("l'equipe 2 est éliminer " + equipe1 + " " + equipe2);}
     }
+}
 }
