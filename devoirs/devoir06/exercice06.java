403e109c6be257faa921e733750cb169c60a86c0
hugo <hugo.maillard@studentfr.ch>
2024-11-18 20:15:58 +0100

    teste

--- devoirs/devoir06/exercice06.java
+++ devoirs/devoir06/exercice06.java
@@ -1,16 +1,17 @@
 package devoirs.devoir06;
 
 public class exercice06 {
-    public static int TAILLE_TABLEAU =20;
+    public static int TAILLE_TABLEAU = 20;
     public static int VALEUR_MAX = 50;
     public static int VALEUR_MIN = 0;
     public static int VALEUR_RECHERCHEE = 7;
+
     public static void main(String[] args) {
         genereTableau(VALEUR_MIN, VALEUR_MAX, TAILLE_TABLEAU);
         int[] tab = new int[TAILLE_TABLEAU];
         for (int i = 0; i < tab.length; i++) {
             tab[i] = (int) (Math.random() * (VALEUR_MAX + VALEUR_MIN - 1)) + 1;
-            System.out.println("contenus du tableau [" + i + "] : [" + tab[i] + "] " );
+            System.out.println("contenus du tableau [" + i + "] : [" + tab[i] + "] ");
         }
         int min = rechercheMin(tab);
         int max = rechercheMax(tab);
@@ -22,43 +23,47 @@ public class exercice06 {
 
         if (position != -1) {
             System.out.println(" le nombre " + VALEUR_RECHERCHEE + " ce trouve a la position " + position);
-        }else{
+        } else {
             System.out.println("le nombre " + VALEUR_RECHERCHEE + " n'est pas dans le tableau ");
         }
     }
-    public static void genereTableau(int entier1, int entier2, int entier3){
-       int[] tab = new int[TAILLE_TABLEAU];
-       for (int i = 0; i < tab.length; i++) {
-            tab[i] =  (int) (Math.random() * (VALEUR_MAX + VALEUR_MIN - 1)) + 1;
+
+    public static void genereTableau(int entier1, int entier2, int entier3) {
+        int[] tab = new int[TAILLE_TABLEAU];
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = (int) (Math.random() * (VALEUR_MAX + VALEUR_MIN - 1)) + 1;
             System.out.println(tab[i]);
         }
 
     }
-public static int rechercheMin(int[] tab){
-    int min = tab[0];
-    for (int i = 0; i < tab.length; i++) {
-        if (tab[i] < min ) {
-            min = tab[i];
+
+    public static int rechercheMin(int[] tab) {
+        int min = tab[0];
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i] < min) {
+                min = tab[i];
+            }
         }
+        return min;
     }
-    return min;
-}
-public static int rechercheMax(int[] tab){
-    int max = tab[0];
-    for (int i = 0; i < tab.length; i++) {
-        if (tab[i] > max) {
-            max = tab[i];
+
+    public static int rechercheMax(int[] tab) {
+        int max = tab[0];
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i] > max) {
+                max = tab[i];
+            }
         }
+
+        return max;
     }
-    
-    return max ;
-}
-public static int rechercheValeur(int[] tab, int valeur){
-    for (int i = 0; i < tab.length; i++) {
-        if (valeur == tab[i]) {
-          return i;  
+
+    public static int rechercheValeur(int[] tab, int valeur) {
+        for (int i = 0; i < tab.length; i++) {
+            if (valeur == tab[i]) {
+                return i;
+            }
         }
+        return -1;
     }
-    return -1;
-}
 }
--- teste.java
+++ teste.java
@@ -1,14 +1,34 @@
 public class teste {
+    public static int MAX = 5;
+    public static int MIN = 0;
+
     public static void main(String[] args) {
-       int nombre = 8;
-       int nombre2 = 3;
-       
-        while (nombre == 8 ) {
-        System.out.println("bonjour");
-       nombre2 = nombre
-    }
-        
+        int equipe1 = (int) (Math.random() * (MAX + MIN - 1)) + 1;
+        int equipe2 = (int) (Math.random() * (MAX + MIN - 1)) + 1;
+        int equipe3 = (int) (Math.random() * (MAX + MIN - 1)) + 1;
+        int equipe4 = (int) (Math.random() * (MAX + MIN - 1)) + 1;
+        // System.out.println(equipe1 + " " + equipe2 + " " + equipe3+" " + equipe4);
+        // premier quart
+        if (equipe1 < equipe2) {
+            System.out.println(" equipe 1 a perdu " + equipe1 + "a" + equipe2);
+            int gagnant1 = (int) (Math.random() * (MAX + MIN - 1)) + 1;
 
+        } else if (equipe1 > equipe2) {
+            System.out.println(" equipe 2 a perdu " + equipe2 + "a" + equipe1);
+            int gagnant1 = (int) (Math.random() * (MAX + MIN - 1)) + 1;
+        }
+        if (equipe3 < equipe4) {
+            System.out.println(" equipe 3 a perdu " + equipe3 + "a" + equipe4);
+            int gagnant2 = (int) (Math.random() * (MAX + MIN - 1)) + 1;
+
+        } else if (equipe1 > equipe2) {
+            System.out.println(" equipe 4 a perdu " + equipe4 + "a" + equipe3);
+            int gagnant2 = (int) (Math.random() * (MAX + MIN - 1)) + 1;
+        }
+        if (gagnant1 < gagnant2) {
+            
+        }
+
+        }
     }
 
-}
